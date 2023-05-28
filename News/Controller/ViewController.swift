8932d82bbcb097e24595bd84bf831f7feece3fe8
Elmar Ibrahimli <elmarni@code.edu.az>
2023-05-28 21:00:20 +0400

    first commit

---
+++ News/Controller/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Elmar Ibrahimli on 28.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
---
+++ News/Resources/ApiCaller.swift
@@ -0,0 +1,13 @@
+//
+//  ApiCaller.swift
+//  News
+//
+//  Created by Elmar Ibrahimli on 28.05.23.
+//
+
+import Foundation
+
+final class ApiCaller {
+    static let shared = ApiCaller()
+    
+}
